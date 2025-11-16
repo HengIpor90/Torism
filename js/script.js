909789c93c6cb3bb0a60ad37ef83533c2b662a95
unknown <hengipor90@gmail.com>
2025-11-16 22:45:47 +0700

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const GetItems = document.getElementById("get");
     const title = document.querySelector(".title");
+    const para = document.querySelector(".para");
     
     const items = ["Home", "About", "Services", "Contact", "Blog"];
     title.textContent = "Welcome to Torism";
-
+    this.parentElement.style.backgroundColor = "lightblue";
+    para.textContent = "Explore the world with us!";
     GetItems.innerHTML = "";
     items.forEach(function(item) {
         const listItem = document.createElement("a");
